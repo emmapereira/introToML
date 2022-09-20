bb87308e33c42a11b49a2e931425dfd6f0464c4e
emma <emmapereira23@gmail.com>
2022-09-20 16:51:45 +0200

    first commit import dataset

---
+++ project1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 16:31:54 2022
+
+@author: emma
+"""
+
+import pandas as pd 
+ 
+dataset = pd.read_csv('https://hastie.su.domains/ElemStatLearn/datasets/SAheart.data?fbclid=IwAR0bnadUy7l7_jwPgJzAW1Dg5RM_JyAKv_doOWxuP2Fx2XpkTAliWHRl73U') 
