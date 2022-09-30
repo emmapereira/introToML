77bd9c7a7ac20f64b26595ea3eac456009a41743
emmapereira <emmapereira23@gmail.com>
2022-09-30 09:36:25 +0200

    pca and plots done

--- project1.py
+++ project1.py
@@ -9,44 +9,46 @@ Created on Tue Sep 20 16:31:54 2022
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import scipy.linalg as linalg
+from sklearn.decomposition import PCA
 
- 
-#import the dataset into a dataframe
+
+# emma
+# import the dataset into a dataframe
 df = pd.read_csv('https://hastie.su.domains/ElemStatLearn/datasets/SAheart.data?fbclid=IwAR0bnadUy7l7_jwPgJzAW1Dg5RM_JyAKv_doOWxuP2Fx2XpkTAliWHRl73U') 
 
+
+# arrange dataset by converting the famhist values and removing the column with row names
 def newdf(df):
 
-    #convert the famhist values into binary
+    # convert the famhist values into binary
     df['famhist'] = (df['famhist'] == 'Present').astype(int)
     
-    #remove the row.names column
+    # remove the row.names column
     newdf = df.drop(columns = 'row.names')
-    
-    #convert the dataframe to numpy arrays  
-    #raw_data = newdf.values  
     
     return newdf
 
 
-#newdf has the dataframe without row.names and binary values in famhist 
+# newdf has the dataframe without row.names and binary values in famhist 
 newdf = newdf(df)
 
-#start making the data matrix X by indexing into data (ignore matrix stuff for now!!)
-#get the values from the columns (we are not taking the chd column)
+# get the values from the columns (we are not taking the chd column)
 cols = range(1, 9) 
-#X = raw_data[:, cols]
 
-#extract attribute names
+# extract attribute names
 attributeNames = np.asarray(df.columns[cols])
-#M = len(attributeNames)
 
+# keep column of chd
+classific = newdf[['chd']]
 
-
-#dataframe without the chd column
+# dataframe without the chd column
 df_noCHD = newdf.drop(columns = 'chd')
 
 
-#print some statistics on each column
+
+# print some statistics on each column
 def datastatistics(df):
 
 
@@ -64,14 +66,28 @@ def datastatistics(df):
 datastatistics(df_noCHD)
 
 
-#boxplots for each attribute
+# boxplots for each attribute
 for column in df_noCHD:
     plt.figure()
     df_noCHD.boxplot([column], patch_artist=True, boxprops=dict(facecolor="lightcyan", color="darkcyan"), medianprops=dict(color="darkcyan"))
     plt.gca().set(ylabel='Frequency')
   
 
-#correlation plot 
+# berta
+# histogram plots 
+newdfp=newdf[newdf['chd']==1] #observations of chp-positive patients
+newdfn=newdf[newdf['chd']==0] #observations of chp-negative patients
+for attribute in attributeNames:
+    plt.figure()
+    newdfn[attribute].plot.hist(grid=True, bins=20, rwidth=0.9, color='#82E0AA')
+    newdfp[attribute].plot.hist(grid=True, bins=20, rwidth=0.6, color='#F1948A')
+    plt.title(attribute)
+    plt.ylabel('Frequency')
+    plt.grid(axis='y', alpha=0.75)
+    
+    
+# saba
+# correlation plot 
 M = len(df_noCHD.columns)
 C = len(df["chd"].unique())
 
@@ -101,7 +117,85 @@ for col1 in df_noCHD.columns: #numerical_col:
 plt.show()
 
 
+# emma
+# PCA computations
+def pca_computations(cleandf):
+    # standarize the data
+    scale= StandardScaler()
+    stanData = scale.fit_transform(cleandf)
+    
+    # PCA by computing SVD of Y
+    U,S,V = linalg.svd(stanData,full_matrices=False)
+    V = V.T
 
+    # Compute variance explained by principal components
+    rho = (S*S) / (S*S).sum() 
 
+    # Project data onto principal component space
+    threshold = 0.9
 
+    # Plot variance explained
+    plt.figure()
+    plt.plot(range(1,len(rho)+1),rho,'x-', color='#82E0AA')
+    plt.plot(range(1,len(rho)+1),np.cumsum(rho),'o-', color='#F1948A')
+    plt.plot([1,len(rho)],[threshold, threshold],'k--')
+    plt.title('Variance explained by principal components');
+    plt.xlabel('Principal component');
+    plt.ylabel('Variance explained');
+    plt.legend(['Individual','Cumulative','Threshold'])
+    plt.grid()
+    plt.show()
+    
+    
+    
+    # Plot two PC coefficients  
+    # choose legends and colors we will use
+    legendStrs = ['PC1', 'PC2']
+    colors = ['#82E0AA','#F1948A']
+    colors = colors*9
+    
+    pcs = [0,1]
+    bw = .2
+    r = np.arange(1,M+1)
+    for i in pcs:    
+        plt.bar(r+i*bw, V[:,i], width=bw, color = colors[i])
+    plt.xticks([1, 2, 3, 4, 5, 6, 7, 8])
+    plt.xlabel('Attributes', fontstyle = 'oblique')
+    plt.ylabel('Component coefficients', fontstyle = 'oblique')
+    plt.legend(legendStrs)
+    plt.title('South African Heart Disease: PCA Component Coefficients', fontweight = 'normal',fontstyle = 'italic')
+    plt.grid()
+    plt.show()
+    
+    
+    
+    # Plot 2D with the first two PCs 
+    pca = PCA(n_components=2)
+    components = pca.fit_transform(stanData)
+    
+    # choose name of the classes, and colors we will use
+    classes = ['noCHD', 'CHD']
+    n_label = int(len(classes))
+    colors = ['#82E0AA','#F1948A']
+    
+    #assign each category with a color
+    cdict = {i: colors[i] for i in range(n_label)}
+    label_dict = {i: classes[i] for i in range(n_label)}
+    components = components * (-1) 
+    
+    # go through the labels and establish their classification 
+    for i in range(n_label):
+        indices = np.where(classific == i)
+        plt.scatter(components[indices, 0], components[indices, 1], color=cdict[i], label=label_dict[i])
+    
+    #Plot
+    plt.legend(loc='best')
+    plt.xlabel('PC1',fontstyle ='oblique')
+    plt.ylabel('PC2',fontstyle = 'oblique')
+    #sns.set()
+    plt.title('PC1 and PC2 in 2 dimensions', fontweight = 'normal',fontstyle = 'italic')
+    plt.grid()
+    plt.show()
+ 
 
+pca_computations(df_noCHD)
